dd5185cd7917e5d9942018b445f1457d1eaca074
John Hawley <jhawley@institute.local>
2017-09-01 22:49:48 -0500

    implemented touching and dragging

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,26 +22,48 @@ public class GameManager : MonoBehaviour {
 
 	void OnDisable(){
 		PlayerController.playerDeath -= PlayerDeath;
+		SceneManager.sceneLoaded -= SceneLoaded;
 	}
 
 	void Update(){
-		
+		StartGame();
+		GameOverToStart();
 	}
 
 	//Functions
 	private void PlayerDeath(){
-		SceneManager.LoadScene("GameOverScene");	
+		SceneManager.LoadScene("GameOverScene");
 	}
 
 	private void SceneLoaded(Scene scene, LoadSceneMode mode){
 		if(scene.name == "GameOverScene"){
 			Debug.Log("Game Over");
-
+		}
+		if(scene.name == "StartScene"){
+			Debug.Log("StartScene");
+		}
+		if(scene.name == "GameScene"){
+			Debug.Log("GameScene");
 		}
 	}
 
 	private void StartGame(){
-		
-		
+		if(Input.GetKeyDown(KeyCode.Space)){
+			if(SceneManager.GetActiveScene().name == "StartScene"){
+				SceneManager.LoadScene("GameScene");
+			}
+		}
 	}
+
+	private void GameOverToStart(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			if(SceneManager.GetActiveScene().name == "GameOverScene"){
+				SceneManager.LoadScene("StartScene");
+			}
+		}
+	}
+
+	
+
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	//public
+	
+	//private
+	bool touched = false;
+
+	//Events and Delegates
 	public delegate void PlayerDeath();
 	public static event PlayerDeath playerDeath = delegate{};
 
@@ -11,6 +17,32 @@ public class PlayerController : MonoBehaviour {
 		if(c.gameObject.GetComponentInParent<EnemyController>()){
 			gameObject.GetComponentInChildren<MeshRenderer>().enabled = false; //On Player Child sphere
 			playerDeath();
+		}
+	}
+
+	void Update() {
+		if(Input.touchCount == 0) return;
+			Touch t = Input.GetTouch(0);
+		UpdateTouch(t);	
+	}
+
+	void UpdateTouch(Touch t){
+		if(t.phase == TouchPhase.Began){
+			Ray r = Camera.main.ScreenPointToRay(t.position);
+			RaycastHit2D hit = Physics2D.GetRayIntersection(r);
+			if (hit.collider != null && hit.collider.gameObject.CompareTag("Player")){
+				Debug.Log("Playerhit");
+				touched = true;
+			}
+		}
+
+		else if(touched && (t.phase == TouchPhase.Moved || t.phase == TouchPhase.Stationary)) {
+			Vector3 touchPos = t.position;
+			touchPos.z = transform.position.z - Camera.main.transform.position.z;
+			transform.position = Camera.main.ScreenToWorldPoint(touchPos);
+		}
+		else if(touched && (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled)){
+			
 		}
 	}
 }
