dd18cce79307a0e12453ada6e1e06c05ccd93c18
John Hawley <jhawley@institute.local>
2017-09-01 19:33:36 -0500

    init commit. created player, enemy, spawner, killbox. All interact on triggre

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D c){
+		if(c.gameObject.GetComponentInParent<EnemyController>()){
+			gameObject.GetComponentInChildren<MeshRenderer>().enabled = false; //On Player Child sphere
+		}
+	}
+}
