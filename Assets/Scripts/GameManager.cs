cd633bd8ad7bb3d9f64237b2afa82b755d2e534a
John Hawley <jhawley@institute.local>
2017-09-01 20:03:25 -0500

    Added playerdeath event / delegate. added game over load delegate for gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	void Awake(){
+		if(instance == null){
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		} 
+		else Destroy(gameObject);
+	}
+
+	void OnEnable(){
+		PlayerController.playerDeath += PlayerDeath;
+		SceneManager.sceneLoaded += SceneLoaded;
+	}
+
+	void OnDisable(){
+		PlayerController.playerDeath -= PlayerDeath;
+	}
+
+	void Update(){
+		
+	}
+
+	//Functions
+	private void PlayerDeath(){
+		SceneManager.LoadScene("GameOverScene");	
+	}
+
+	private void SceneLoaded(Scene scene, LoadSceneMode mode){
+		if(scene.name == "GameOverScene"){
+			Debug.Log("Game Over");
+
+		}
+	}
+
+	private void StartGame(){
+		
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public delegate void PlayerDeath();
+	public static event PlayerDeath playerDeath = delegate{};
+
 	void OnTriggerEnter2D(Collider2D c){
 		if(c.gameObject.GetComponentInParent<EnemyController>()){
 			gameObject.GetComponentInChildren<MeshRenderer>().enabled = false; //On Player Child sphere
+			playerDeath();
 		}
 	}
 }
